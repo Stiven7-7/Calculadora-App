85ebe19b2c42192e04d7c6c0dddd484161172302
Brayan Pinzon <58951748+Stiven7-7@users.noreply.github.com>
2023-10-20 20:26:39 -0500

    Cambios visuales y se inicia logica en la funcionalidad

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var tvRes: TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvRes=findViewById(R.id.tvRes)
+    }
+    fun calcular(view : View){
+        var boton = view as Button
+        var textButton = boton.text.toString()
+        var concatenar = tvRes?.text.toString() + textButton
+        var mostrar = quitarCerosIzq(concatenar)
+
+        if(textButton == "="){
+            var result = 0.0
+        }
+
+    }
+    fun quitarCerosIzq(str : String):String{
+        var i = 0
+        while (i < str.length && str[i] == '0') i++
+        var sb = StringBuffer(str)
+        sb.replace(0, i, "")
+        return sb.toString()
     }
 }
